cf324f072bc6cd88342096a2d65aace7b69e47d2
Narongsak Keawmanee <klogic@hotmail.co.th>
2018-06-16 20:32:09 +0700

    get posts and show it

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -6,11 +6,29 @@ class PostIndex extends Component{
   componentDidMount(){
     this.props.fetchPosts()
   }
+
+  returnPosts(){
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={ post.id } >
+          { post.title } 
+        </li>
+      )
+    })
+  }
   render(){
     return(
-      <div>Post Index</div>
+      <div>
+        <h3>Posts</h3>
+        <ul className="list-group">
+          { this.returnPosts() }
+        </ul>
+      </div>
     );
   }
 }
 
-export default connect(null, { fetchPosts })(PostIndex);
+function mapStatetoProps(state) {
+  return { posts: state.posts }
+}
+export default connect(mapStatetoProps, { fetchPosts })(PostIndex);
