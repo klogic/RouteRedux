bf307706e443339b1b0a58a796a17e7fa858fe16
Narongsak Keawmanee <klogic@hotmail.co.th>
2018-06-24 15:26:26 +0700

    post show action and reducer

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,10 +1,20 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 
 export default function(state = {}, action){
   switch (action.type) {
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
+    case FETCH_POST:
+      /* 
+        es5 return state with object id: value
+        const post = action.payload.data;
+        const newState = { ... state, }
+        newState[post.id] = post;
+        return newState;
+      */
+      return { ...state, [action.payload.data.id]: action.payload.data }
+
     default:
       return state;
   }
