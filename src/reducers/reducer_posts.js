e37813392545e2d04ec772632abb22b172868232
Narongsak Keawmanee <klogic@hotmail.co.th>
2018-06-26 13:50:22 +0700

    remove comment code

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -8,15 +8,7 @@ export default function(state = {}, action){
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
     case FETCH_POST:
-      /* 
-        es5 return state with object id: value
-        const post = action.payload.data;
-        const newState = { ... state, }
-        newState[post.id] = post;
-        return newState;
-      */
       return { ...state, [action.payload.data.id]: action.payload.data }
-
     default:
       return state;
   }
